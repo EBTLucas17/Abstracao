606e8ba7c47bf0eea4ab86ed96d7f19f8eaad378
Lucas Batista <lucasbatista12312@gmail.com>
2021-08-24 15:25:47 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracao
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Notebook sony = new Notebook("Sony Vaio");
+            sony.Porta1 = new Tablet();
+            var tablet = new Tablet();
+            tablet.Plugar();
+            sony.Porta2 = new Mouse();
+            var mouse = new Mouse();
+            mouse.Plugar();
+            sony.Porta3 = new Teclado();
+            var teclado = new Teclado();
+            teclado.Plugar();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tablet.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Tablet : USB
+{
+    public override void Plugar()
+    {
+        Console.WriteLine("Tablet plugado..");
+    }
+}
